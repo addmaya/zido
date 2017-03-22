303163901cca1edcda4ef8f9ac0148a94f100544
Addmaya <admin@addmaya.com>
2017-03-22 18:24:51 +0300

    update

--- functions.php
+++ functions.php
@@ -405,7 +405,7 @@
 	    global $post;
 	    $brief = get_field('pmt_brief', $post->ID);
 	    if( has_post_thumbnail( $post->ID ) ) {
-	        $content = '<p>' . get_post_thumb_medium() . $brief . '</p>' . $content;
+	        $content = '<p><img src="' . get_post_thumb_medium() .'" />'. $brief . '</p>' . $content;
 	    }
 	    return $content;
 	}
